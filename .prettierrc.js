dc79e38d58f02fc797f03c048d7889eaeb2ee4df
pajecawav <pajecawav@gmail.com>
2024-02-17 16:20:54 +0300

    chore: update prettier and renovate configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,1 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-	printWidth: 100,
-	useTabs: true,
-	tabWidth: 4,
-	arrowParens: "avoid",
-	overrides: [
-		{
-			files: "*.md",
-			options: {
-				useTabs: false,
-			},
-		},
-	],
-};
+module.exports = "@pajecawav/prettier-config";
